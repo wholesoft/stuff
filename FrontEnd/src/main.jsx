f8208121ba95521e61055ad2ff13facfa628c061
Erik <69864741+eze-gh@users.noreply.github.com>
2023-03-31 10:41:53 -0700

    layout updates

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './context/AuthProvider'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./context/AuthProvider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css"; 
-import 'primeflex/primeflex.css';
-import './styles.css'
+import "primereact/resources/themes/lara-light-indigo/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeicons/primeicons.css"
+import "primeflex/primeflex.css"
+import "./styles.css"
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
- <AuthProvider> 
-      <Routes>
-        <Route path="/*" element={<App />} />
-    </Routes> 
-    </AuthProvider>
-
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
     {/*<ReactQueryDevtools />*/}
-    </QueryClientProvider>
+  </QueryClientProvider>
 
   //</React.StrictMode>,
 )
- 
