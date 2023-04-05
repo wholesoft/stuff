efe6376a1b523384e4f5c20724a75675fdb9c753
Erik <69864741+eze-gh@users.noreply.github.com>
2023-04-05 16:07:02 -0700

    Update credentials.js

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -1,12 +1,14 @@
-import allowedOrigins from '../config/allowedOrigins.js';
+import dotenv from "dotenv"
+dotenv.config()
+
+const allowedOrigins = [process.env.CORS_ALLOWED_ORIGIN]
 
 const credentials = (req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true)
-    }
-    next();
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Credentials", true)
+  }
+  next()
 }
 
 export default credentials
-
