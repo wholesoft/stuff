a43f0c54045554fe6caf421fc72dcf0e0d36aad2
Erik <69864741+eze-gh@users.noreply.github.com>
2023-04-07 09:21:18 -0700

    logging

--- backend/mail.js
+++ backend/mail.js
@@ -1,8 +1,18 @@
 import sgMail from "@sendgrid/mail"
 import dotenv from "dotenv"
 import nodemailer from "nodemailer"
+import mysql from "mysql2"
 
 dotenv.config()
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise()
 
 /*
 Using SendGrid for now.
@@ -91,10 +101,11 @@ export default async function sendEmail(mailTo, subject, textBody, htmlBody) {
   // Returns { 'success': true/false , 'message': '' }
   let success = false
   let message = ""
+  let mailFrom = process.env.EMAIL_FROM
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
     to: mailTo, // Change to your recipient
-    from: process.env.EMAIL_FROM, // Change to your verified sender
+    from: mailFrom,
     subject: subject,
     text: textBody,
     html: htmlBody,
@@ -112,16 +123,42 @@ export default async function sendEmail(mailTo, subject, textBody, htmlBody) {
       message = "Error sending email."
       console.log(error)
     })
+  logEmail(mailTo, mailFrom, subject, htmlBody, message, "sendgrid")
   return { success: success, message: message }
 }
 
-/*
-console.log(
-  await sendEmail(
-    "erikthompson@yandex.com",
-    "Test Subject2",
-    "Text Message",
-    "<b>HTML Message</b>"
+export async function logEmail(
+  mail_to,
+  mail_from,
+  subject,
+  body,
+  notes,
+  service
+) {
+  const result = await pool.query(
+    `
+         INSERT INTO EmailLog (mail_to, mail_from, subject, body, notes, service) 
+         VALUES (?,?,?,?,?,?)
+         `,
+    [mail_to, mail_from, subject, body, notes, service]
   )
-)
+  return result
+}
+
+/*
+
++-----------------+--------------+------+-----+-------------------+-------------------+
+| Field           | Type         | Null | Key | Default           | Extra             |
++-----------------+--------------+------+-----+-------------------+-------------------+
+| id              | int          | NO   | PRI | NULL              | auto_increment    |
+| ‘user_id’       | int          | YES  |     | NULL              |                   |
+| ‘mail_to’       | varchar(255) | YES  |     | NULL              |                   |
+| ‘mail_from’     | varchar(255) | YES  |     | NULL              |                   |
+| ‘subject’       | varchar(255) | YES  |     | NULL              |                   |
+| ‘body’          | text         | YES  |     | NULL              |                   |
+| notes           | text         | YES  |     | NULL              |                   |
+| ‘service’       | varchar(255) | YES  |     | NULL              |                   |
+| created         | datetime     | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
++-----------------+--------------+------+-----+-------------------+-------------------+
+
 */
